56a77018aeb3ca8d6fadf7d2b50767b7314b60cd
KotaMorikawa <koke.kokota@gmail.com>
2019-07-13 22:08:07 +0900

    7/14-1

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,6 +4,7 @@ class MessagesController < ApplicationController
     def index
         @message = Message.new
         @messages = @group.messages.includes(:user)
+        users = User.where("id not ?", nil)
     end
 
     def create
