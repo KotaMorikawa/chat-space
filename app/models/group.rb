509b24f846b6c07a76488451f5f4c0842330b789
KotaMorikawa <koke.kokota@gmail.com>
2019-07-13 20:42:48 +0900

    7/13-5

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,12 @@ class Group < ApplicationRecord
     has_many :group_users
     has_many :messages
     validates :name, presence: true, uniqueness: true
+
+    def show_last_message
+        if (last_messages = messages.last).present?
+            last_messages.body? ? last_messages.body : '画像が投稿されています'
+        else
+            'まだメッセージはありません'
+        end
+    end
 end
