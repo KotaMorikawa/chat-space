9d86d157de309f2579f7471c3c6914b853df624b
KotaMorikawa <koke.kokota@gmail.com>
2019-07-17 17:06:50 +0900

    7/17-2

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -13,7 +13,7 @@ $(function() {
                         </div>
                         <div class="chat-main__messages__content__text">
                             ${body}
-                            ${image}
+                        ${image}
                         </div>
                     </div>`
         return html;
