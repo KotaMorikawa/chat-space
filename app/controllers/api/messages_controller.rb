0f9976557b68fde97d4ebd8263e21e002d05e60c
KotaMorikawa <koke.kokota@gmail.com>
2019-07-20 17:59:20 +0900

    7/20-2

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -2,7 +2,11 @@ class Api::MessagesController < ApplicationController
 
     def index
         @group = Group.find(params[:group_id])
-        @messages = @group.messages.includes(:user).where('id > ?', params[:id])
+        if (params[:id] == nil)
+            @messages = @group.messages.includes(:user)
+        else
+            @messages = @group.messages.includes(:user).where('id > ?', params[:id])
+        end
     end
 
 end
